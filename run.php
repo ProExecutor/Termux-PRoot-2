75ed3de9cce5053f6ad76df484a2efc55d255038
ProExecutor <123886692+ProExecutor@users.noreply.github.com>
2023-12-14 05:23:55 +0300

    Update run.php

--- run.php
+++ run.php
@@ -1,6 +1,6 @@
 <?php
 // Run the bash script and store the output in a variable
-$output = shell_exec("PATH/TO/YOUR/script.sh");
+$output = shell_exec("main.sh");
 
 // Echo the output to the browser
 echo "<pre>$output</pre>";
